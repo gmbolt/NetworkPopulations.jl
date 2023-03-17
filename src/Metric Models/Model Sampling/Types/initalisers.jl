83aa294e3e12cdfa8a8e8ffd4c47d58a20d22d1f
George Bolt <g.bolt@lancaster.ac.uk>
2023-03-17 16:48:35 +0000

    Style changes

--- src/Metric Models/Model Sampling/Types/initalisers.jl
+++ src/Metric Models/Model Sampling/Types/initalisers.jl
@@ -17,30 +17,30 @@ abstract type McmcInitialiser end
 """
 struct InitMode <: McmcInitialiser
     function InitMode()
-        return new() 
-    end 
-end 
+        return new()
+    end
+end
 
 function get_init(
     initiliaser::InitMode,
-    model::Union{SIS,SIM}, 
-    )
+    model::Union{SIS,SIM},
+)
     return model.mode
-end 
+end
 
 struct InitRandEdit <: McmcInitialiser
     δ::Int
-    function InitRandEdit(δ::Int) 
+    function InitRandEdit(δ::Int)
         return new(δ)
-    end 
-end 
+    end
+end
 
 function perturb(
     S::InteractionSequence,
     V::AbstractArray,
-    K_inner::DimensionRange, 
+    K_inner::DimensionRange,
     δ::Int
-    )
+)
 
     S_init = deepcopy(S)
     N = length(S)
@@ -51,22 +51,22 @@ function perturb(
 
     rem_edits = δ
 
-    for i in 1:N 
-        if i == N 
+    for i in 1:N
+        if i == N
             δ_tmp = rem_edits
-        else 
-            p = 1/(N-i+1)
+        else
+            p = 1 / (N - i + 1)
             δ_tmp = rand(Binomial(rem_edits, p))
-        end 
+        end
 
         if δ_tmp == 0
-            continue 
+            continue
         else
 
-            
+
             n = length(S[i])
-            d = rand(max(0, ceil(Int, (n + δ_tmp + - K_inner.u)/2)):min(n-K_inner.l, δ_tmp))
-            m = n + δ_tmp - 2*d
+            d = rand(max(0, ceil(Int, (n + δ_tmp + -K_inner.u) / 2)):min(n - K_inner.l, δ_tmp))
+            m = n + δ_tmp - 2 * d
 
             ind_del_v = view(ind_del, 1:d)
             ind_add_v = view(ind_add, 1:(δ_tmp-d))
@@ -78,42 +78,42 @@ function perturb(
 
             delete_insert!(S_init[i], ind_del_v, ind_add_v, vals_v)
 
-        end 
+        end
 
-        rem_edits -= δ_tmp 
+        rem_edits -= δ_tmp
 
-        if rem_edits == 0 
-            break 
-        end 
-    end 
+        if rem_edits == 0
+            break
+        end
+    end
 
     return S_init
 
-end 
+end
 
 
 
 function get_init(
     initialiser::InitRandEdit,
     model::Union{SIS,SIM}
-    )
+)
     return perturb(model.mode, model.V, model.K_inner, initialiser.δ)
-end 
+end
 
 struct InitRandIns <: McmcInitialiser
     δ::Int
-    function InitRandIns(δ::Int) 
+    function InitRandIns(δ::Int)
         return new(δ)
-    end 
-end 
+    end
+end
 
 
 function perturb_ins(
     S::InteractionSequence,
     V::AbstractArray,
-    K_inner::DimensionRange, 
+    K_inner::DimensionRange,
     δ::Int
-    )
+)
 
     S_init = deepcopy(S)
     N = length(S)
@@ -122,48 +122,48 @@ function perturb_ins(
 
     rem_edits = δ
 
-    for i in 1:N 
-        if i == N 
+    for i in 1:N
+        if i == N
             δ_tmp = rem_edits
-        else 
-            p = 1/(N-i+1)
+        else
+            p = 1 / (N - i + 1)
             δ_tmp = rand(Binomial(rem_edits, p))
-        end 
+        end
 
         if δ_tmp == 0
-            continue 
+            continue
         else
-            
+
             n = length(S[i])
-            m = n + δ_tmp 
+            m = n + δ_tmp
             if m > K_inner.u
-                δ_tmp = (K_inner.u-n)
+                δ_tmp = (K_inner.u - n)
                 m = n + δ_tmp
-            end 
+            end
             ind_add_v = view(ind_add, 1:δ_tmp)
             StatsBase.seqsample_a!(1:m, ind_add_v)
-            
-            for j in ind_add_v 
+
+            for j in ind_add_v
                 insert!(S_init[i], j, rand(V))
-            end 
+            end
 
-        end 
+        end
 
-        rem_edits -= δ_tmp 
+        rem_edits -= δ_tmp
 
-        if rem_edits == 0 
-            break 
-        end 
-    end 
+        if rem_edits == 0
+            break
+        end
+    end
 
     return S_init
 
-end 
+end
 
 function get_init(
     initialiser::InitRandIns,
     model::Union{SIS,SIM}
-    )
+)
     return perturb_ins(model.mode, model.V, model.K_inner, initialiser.δ)
-end 
+end
 
