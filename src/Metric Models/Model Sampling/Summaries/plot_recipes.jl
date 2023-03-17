312d5d3e1352ed5fd856da39d19ef8bbaabbd16d
George Bolt <g.bolt@lancaster.ac.uk>
2023-03-17 16:48:16 +0000

    Move summary plot

--- src/Metric Models/Model Sampling/Summaries/plot_recipes.jl
+++ src/Metric Models/Model Sampling/Summaries/plot_recipes.jl
@@ -58,30 +58,3 @@ end
 end
 
 
-@userplot SummaryPlot
-@recipe function f(h::SummaryPlot)
-    output = h.args  # A mcmc_output type
-    model = output[1].model
-    sample = output[1].sample
-    layout := (3, 1)
-    legend --> false
-    xguide := "Sample"
-    size --> (800, 600)
-    margin --> 5mm
-
-    @series begin
-        seriestype := :line
-        yguide --> "Dist. from Mode"
-        map(x -> model.dist(model.mode, x), sample)
-    end
-    @series begin
-        seriestype := :line
-        yguide --> "Num. of Paths"
-        map(length, sample)
-    end
-    @series begin
-        seriestype := :line
-        yguide --> "Avg. Path Len."
-        map(x -> mean(length.(x)), sample)
-    end
-end
