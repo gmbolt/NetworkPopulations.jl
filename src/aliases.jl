b3c62d7ac4d57d2bd67a444d7b8aed307c56f6a4
George Bolt <g.bolt@lancaster.ac.uk>
2023-04-11 19:46:43 +0100

    Add new aliases

--- src/aliases.jl
+++ src/aliases.jl
@@ -1,6 +1,7 @@
-export Path, InteractionSequence, InteractionSequenceSample
+export Path, InteractionSequence, InteractionSequenceSample, InterSeq, InterSeqSample
 
-const Path{T} = Vector{T} where {T<:Union{Int, String}} 
-const InteractionSequence{T} = Vector{Vector{T}} where {T<:Union{Int, String}}
-const InteractionSequenceSample{T} = Vector{Vector{Vector{T}}} where {T<:Union{Int, String}}
-
+const Path{T} = Vector{T} where {T<:Union{Int,String}}
+const InteractionSequence{T} = Vector{Vector{T}} where {T<:Union{Int,String}}
+const InterSeq{T} = Vector{Vector{T}} where {T<:Union{Int,String}}
+const InteractionSequenceSample{T} = Vector{Vector{Vector{T}}} where {T<:Union{Int,String}}
+const InterSeqSample{T} = Vector{Vector{Vector{T}}} where {T<:Union{Int,String}}
